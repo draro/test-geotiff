a626c7c7f3f4e2e8c884081fee9e2ea90506a820
Davide Raro <58738792+draro@users.noreply.github.com>
2020-03-18 06:33:42 -0400

    Initial commit

---
+++ src/Map.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef } from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import L from "leaflet";
+// const chroma = require("chroma-js");
+// import * as d3 from "d3";
+
+const position = [51.505, -0.09];
+export default () => {
+  const mapRef = useRef();
+
+  useEffect(() => {
+    const map = mapRef.current.leafletElement;
+
+    const script = document.createElement("script");
+    script.src =
+      "https://ihcantabria.github.io/Leaflet.CanvasLayer.Field/dist/leaflet.canvaslayer.field.js";
+    script.async = true;
+    script.onload = () => loaded(map);
+
+    document.body.appendChild(script);
+  }, []);
+
+  const loaded = map => {
+    var tiff = "temp.tiff";
+    fetch(tiff)
+      .then(r => r.arrayBuffer())
+      .then(function(buffer) {
+        var ndvi = L.ScalarField.fromGeoTIFF(buffer);
+        ndvi.params.xllCorner = ndvi.params.xllCorner / 100000;
+        ndvi.params.yllCorner = ndvi.params.yllCorner / 100000;
+        ndvi.xllCorner = ndvi.xllCorner / 100000;
+        ndvi.yllCorner = ndvi.yllCorner / 100000;
+        ndvi.xurCorner = ndvi.xurCorner / 100000;
+        ndvi.yurCorner = ndvi.yurCorner / 100000;
+        ndvi.cellXSize = ndvi.cellXSize / 100000;
+        ndvi.cellYSize = ndvi.cellYSize / 100000;
+        ndvi.isContinuous = false;
+        ndvi.longitudeNeedsToBeWrapped = false;
+
+        let layer = L.canvasLayer
+          .scalarField(ndvi, {
+            // color: chroma.scale("Spectral").domain(ndvi.range.reverse()),
+            opacity: 1,
+            inFilter: v => v !== 0
+          })
+          .addTo(map);
+        layer.on("click", function(e) {
+          if (e.value !== null) {
+            let v = e.value.toFixed(2);
+            let html = `<span class = "popupText"> Temperature ${v} °C</span>`;
+            L.popup()
+              .setLatLng(e.latlng)
+              .setContent(html)
+              .openOn(map);
+          }
+        });
+
+        map.fitBounds(layer.getBounds());
+      });
+  };
+
+  return (
+    <Map ref={mapRef} zoom={2} style={{ height: "100vh" }}>
+      <TileLayer
+        url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup.
+          <br />
+          Easily customizable.
+        </Popup>
+      </Marker>
+    </Map>
+  );
+};
